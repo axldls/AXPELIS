b091face0d138a53d638437b0ac4c932c6c51df8
andresdlss <andresaxeldelossantos@gmail.com>
2026-04-28 16:51:50 -0300

    elimino bubbles y cambio a hashrouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,21 @@
 import './App.css'
 import Bubbles from './components/Bubbles';
-import { Route, Routes, BrowserRouter } from 'react-router-dom';
+import { Route, Routes, HashRouter } from 'react-router-dom';
 import MainPage from './components/MainPage';
 import SingleMovie from './components/SingleMovie';
+import { HashRouter } from 'react-router-dom';
 
 
 function App() {
 
   return (
       <div className='app'>
-        <Bubbles/>
-        <BrowserRouter basename='/AXPELIS'>
+        <HashRouter basename='/AXPELIS'>
             <Routes>
               <Route path="/" element={<MainPage/>} />
               <Route path="/movies/:id" element={<SingleMovie/>} />
             </Routes>
-        </BrowserRouter>
+        </HashRouter>
       </div>
   )
 }
